fcf638563a56b69289a65a548c2045c669763bcf
Kristina Ryzhova <zelbusinka@yandex.ru>
2024-07-02 21:07:14 +0300

    button is moving

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,12 +1,20 @@
+import { useState } from 'react';
+
 import { ArrowButton } from 'components/arrow-button';
 import { Button } from 'components/button';
 
 import styles from './ArticleParamsForm.module.scss';
 
 export const ArticleParamsForm = () => {
+	const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+	function toggleMenu() {
+		setIsMenuOpen(!isMenuOpen);
+	}
+
 	return (
 		<>
-			<ArrowButton />
+			<ArrowButton onClick={toggleMenu} isOpen={isMenuOpen} />
 			<aside className={styles.container}>
 				<form className={styles.form}>
 					<div className={styles.bottomContainer}>
