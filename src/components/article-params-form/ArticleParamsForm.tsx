48f0ad2684931d02a23a565a66aa6c940dd79e7f
Kristina Ryzhova <zelbusinka@yandex.ru>
2024-07-03 21:07:56 +0300

    font change and reset

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,30 +1,68 @@
-import { useState } from 'react';
+import { useState, FormEvent } from 'react';
 import clsx from 'clsx';
 
 import { ArrowButton } from 'components/arrow-button';
 import { Button } from 'components/button';
+import { Text } from '../text';
 
 import styles from './ArticleParamsForm.module.scss';
+import { Select } from '../select';
+import { ArticleStateType, OptionType, defaultArticleState, fontFamilyOptions } from 'src/constants/articleProps';
 
-export const ArticleParamsForm = () => {
+type TArticleFormProps = {
+	setNewParams: (newState: ArticleStateType)=> void
+}
 
-	const [isMenuOpen, setIsMenuOpen] = useState(false);
+export const ArticleParamsForm = ({ setNewParams }: TArticleFormProps) => {
 
-	function toggleMenu() {
-		setIsMenuOpen(!isMenuOpen);
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    function toggleMenu() {
+        setIsMenuOpen(!isMenuOpen);
+    }
+
+	const [articleParams, setArticleParams] = useState(defaultArticleState);
+
+	function handleOptionChange(key: string, option: OptionType) {
+		setArticleParams({ ...articleParams, [key]: option });
 	}
 
-	return (
-		<>
-			<ArrowButton onClick={toggleMenu} isOpen={isMenuOpen} />
-			<aside className={clsx(styles.container, { [styles.container_open]: isMenuOpen })}>
-				<form className={styles.form}>
-					<div className={styles.bottomContainer}>
-						<Button title='Сбросить' type='reset' />
-						<Button title='Применить' type='submit' />
-					</div>
-				</form>
-			</aside>
-		</>
-	);
+	function handleSubmitOptions(e: FormEvent<HTMLFormElement>) {
+		e.preventDefault();
+		setNewParams(articleParams);
+	}
+
+    function handleResetOptions(e: FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+        setNewParams(defaultArticleState);
+    }
+
+    return (
+        <>
+            <ArrowButton onClick={toggleMenu} isOpen={isMenuOpen} />
+            <aside className={clsx(styles.container, { [styles.container_open]: isMenuOpen })}>
+                <form
+					className={styles.form}
+					onSubmit={handleSubmitOptions}
+					onReset={handleResetOptions}>
+                    <Text as='h2' weight={800} size={31} uppercase>
+                        Задайте параметры
+                    </Text>
+                    <Select
+                        title={'шрифт'}
+                        options={fontFamilyOptions}
+						selected={articleParams.fontFamilyOption}
+						onChange={(selected) => {
+							handleOptionChange('fontFamilyOption', selected);
+						}} />
+                    
+                    <div className={styles.bottomContainer}>
+                        <Button title='Сбросить' type='reset' />
+                        <Button title='Применить' type='submit' />
+                    </div>
+                </form>
+            </aside>
+        </>
+    );
 };
+
